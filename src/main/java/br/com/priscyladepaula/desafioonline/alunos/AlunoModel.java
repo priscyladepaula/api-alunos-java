38502885b82f1adab2c418c7b4c5047755414fb3
Priscyla de Paula <30823653+priscyladepaula@users.noreply.github.com>
2023-10-21 01:03:02 -0300

    Add importação Schema

--- src/main/java/br/com/priscyladepaula/desafioonline/alunos/AlunoModel.java
+++ src/main/java/br/com/priscyladepaula/desafioonline/alunos/AlunoModel.java
@@ -1,5 +1,6 @@
 package br.com.priscyladepaula.desafioonline.alunos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
